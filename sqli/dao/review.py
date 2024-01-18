035d76cb644b35820987f34f292f4f120e7e2014
William Wissemann <william.wissemann@harness.io>
2024-01-18 12:56:59 -0500

    update review.py

--- sqli/dao/review.py
+++ sqli/dao/review.py
@@ -38,4 +38,3 @@ class Review(NamedTuple):
         domain = input("Enter the Domain: ")
         output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
         print(output)
-        eval("True")
